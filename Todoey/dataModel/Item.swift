46823da7ff3637c650d91e7199b106b9f51a3a7b
User <user@users-macbook-pro.local>
2019-06-06 15:36:14 +0930

    convert to MVC design patterns

---
+++ Todoey/dataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by User on 5/6/19.
+//  Copyright © 2019 kymsemmler. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    }
